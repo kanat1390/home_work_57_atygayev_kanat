4515f3c5527f9a00581ffeee0ea3f8755aa55fb4
iprogrammer1390 <iprogrammer1390@gmail.com>
2022-10-06 19:27:15 +0600

    Удалены лишние комментарии

--- src/tasks/models/task.py
+++ src/tasks/models/task.py
@@ -6,7 +6,6 @@ from ckeditor.fields import RichTextField
 
 class Task(models.Model):
     summary = models.CharField(verbose_name='Заголовок', max_length=200)
-    # description = models.TextField(verbose_name='Описание', null=True, blank=True, max_length=2000)
     description = RichTextField(verbose_name='Описание', null=True, blank=True, max_length=2000)
     status = models.ForeignKey(verbose_name='Статус', to='tasks.Status', on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add=True)
